0eaac249e6e1c65bdda0eb75fe87cf9ff306facd
SkyKa <sky,ka@mail.ru>
2019-12-30 10:47:14 +0300

    All

--- README.md
+++
@@ -1,2 +0,0 @@
-# WebPackConfig
-Test webpack config
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// BOOTSTRAP CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// CSS
+import './assets/css/main.css';
+
+
+// JQUERY
+import $ from 'jquery';
+
+// SWIPER
+import Swiper from 'swiper';
+
+// JS
+import './js/'
